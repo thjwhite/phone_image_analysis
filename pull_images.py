17dbe991d1d7c16a081c0582cab42a07c04ea79d
tj.white <thjwhite@gmail.com>
2017-06-27 22:41:10 +0000

    add more parsing to script, download the images

--- pull_images.py
+++ pull_images.py
@@ -2,10 +2,13 @@ import requests
 import os
 import sys
 import json
+import uuid
+import hashlib
+import time
 
 GOOGLE_SEARCH_URL='https://www.googleapis.com/customsearch/v1'
-DEBUG=False
-WRITE_RESPONSE=True
+DEBUG=True
+WRITE_RESPONSE=False
 
 def main():
     custom_search_engine_id = os.environ.get('GOOGLE_API_CX')
@@ -31,13 +34,45 @@ def main():
         if WRITE_RESPONSE:
             with open('.sample_google_output', 'w+') as f:
                 json.dump(payload, f)
+
+    if not os.path.exists('.google_api_queries'):
+        os.makedirs('.google_api_queries')
+    if not os.path.exists('.images'):
+        os.makedirs('.images')
+    t_process = time.time()
+    print('====================')
+    print(t_process)
+    print('====================')
+    with open ('.google_api_queries/%s' % t_process, 'w') as f:
+        json.dump(payload, f)
     
     for search_item in payload['items']:
         print('=================================')
         print(search_item['link'])
-        # todo - null check
-        print([img['url'] for img in search_item['pagemap']['imageobject']])
+        urls = list()
+        if 'pagemap' in search_item and 'imageobject' in search_item['pagemap']:
+            imgs = search_item['pagemap']['imageobject']
+            for img in imgs:
+                if 'url' in img:
+                    urls.append(img['url'])
+                elif 'contenturl' in img:
+                    urls.append(img['contenturl'])
+                else:
+                    print('no images! probably missed something')
+
+        if len(urls) == 0:
+            continue
         
+        print('downloading images')
+        for url in urls:
+            img_resp = requests.get(url)
+            content = img_resp.content
+            h = hashlib.sha256(content).hexdigest()
+            u = str(uuid.uuid4())
+            with open('.images/%s' % u, 'wb') as f:
+                f.write(img_resp.content)
+            print('wrote uuid=%s sha256=%s' % (u, h))
+            
     
 
 if __name__ == "__main__":
