15a5a78c3ee804ff404938a71757d39b5849a6e4
tj.white <thjwhite@gmail.com>
2017-07-11 18:39:45 -0700

    pylint

--- pull_images.py
+++ pull_images.py
@@ -1,8 +1,6 @@
 """
 # todo - make multiple queries to get around 10 page limit (iphone 6 vs iphone 7)
-# todo - pylint
 """
-import requests
 import os
 import sys
 import json
@@ -10,18 +8,17 @@ import uuid
 import hashlib
 import time
 import traceback
-import lmdb
+
 from urllib.parse import urlparse
 
-GOOGLE_SEARCH_URL='https://www.googleapis.com/customsearch/v1'
-DEBUG=False
-WRITE_RESPONSE=False
+import lmdb
+import requests
 
-DEBUG_SAMPLE_GOOGLE_API_OUTPUT_DIR='.sample_google_output'
-GOOGLE_API_OUTPUT_DIR='.google_api_queries'
-IMAGE_REPO_DIR='.images'
 
-LMDB_FILE='.registrar'
+GOOGLE_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1'
+GOOGLE_API_OUTPUT_DIR = '.google_api_queries'
+IMAGE_REPO_DIR = '.images'
+LMDB_FILE = '.registrar'
 
 
 def grab_auth():
@@ -33,12 +30,12 @@ def grab_auth():
     return custom_search_engine_id, google_api_key
 
 
-def generate_iphone_search_params(cx, api_key, start=1):
+def generate_iphone_search_params(engine_id, api_key, start=1):
     """
     parameters for a search for iphones
     """
     params = dict()
-    params['cx'] = cx  # custom search engine identifier
+    params['cx'] = engine_id  # custom search engine identifier
     params['key'] = api_key  # api key
     params['searchType'] = 'image'  # tell google we want images, not links
     params['q'] = 'iphone'  # our search term
@@ -48,23 +45,16 @@ def generate_iphone_search_params(cx, api_key, start=1):
 
 def execute_request(custom_search_engine_id, google_api_key, start=1):
     """
-    execute request or load debug payload, save output in special location if enabled
+    execute request to google for the results
     """
-    if DEBUG:
-        with open(DEBUG_SAMPLE_GOOGLE_API_OUTPUT_DIR) as f:
-            payload = json.load(f)
-    else:
-        params = generate_iphone_search_params(custom_search_engine_id, google_api_key, start)
-        response = requests.get(GOOGLE_SEARCH_URL, params=params)
-        print(response)
-        if response.status_code != 200:
-            print(params)
-            print(response.content)
-            raise ValueError('could not successfully query google')
-        payload = response.json()
-        if WRITE_RESPONSE:
-            with open(DEBUG_SAMPLE_GOOGLE_API_OUTPUT_DIR, 'w+') as f:
-                json.dump(payload, f)
+    params = generate_iphone_search_params(custom_search_engine_id, google_api_key, start)
+    response = requests.get(GOOGLE_SEARCH_URL, params=params)
+    print(response)
+    if response.status_code != 200:
+        print(params)
+        print(response.content)
+        raise ValueError('could not successfully query google')
+    payload = response.json()
     return payload
 
 
@@ -81,26 +71,35 @@ def get_urls(payload):
     return urls
 
 
-def write_new(url, content, h, lmdb_env):
+def write_new(url, content, hash_val, lmdb_env):
     """
     writes out a new image
     """
-    u = uuid.uuid4()
-    with open('%s/%s' % (IMAGE_REPO_DIR, u), 'wb') as f:
-        f.write(content)
-    print('wrote url=%s\n      uuid=%s\n      sha256=%s\n' % (url, str(u), h.hexdigest()))
+    uuid_val = uuid.uuid4()
+    with open('%s/%s' % (IMAGE_REPO_DIR, uuid_val), 'wb') as image_file:
+        image_file.write(content)
+    print('wrote url=%s\n      uuid=%s\n      sha256=%s\n' %
+          (url, str(uuid_val), hash_val.hexdigest()))
     with lmdb_env.begin(write=True) as txn:
         # map our hash value to the image uuid
-        txn.put(h.digest(), u.bytesq)
+        txn.put(hash_val.digest(), uuid_val.bytes)
 
 
-def dedup(h, lmdb_env):
+def dedup(hash_val, lmdb_env):
+    """
+    returns whether of not this hash is already in our image repository
+    """
     with lmdb_env.begin() as txn:
-        image_uuid = txn.get(h.digest())
+        image_uuid = txn.get(hash_val.digest())
         return image_uuid is not None
 
 
 def process_urls(urls, lmdb_env):
+    """
+    urls is a list of tuples (url, thumbnail). We try the url first,
+    but if it fails to download it we can use the thumbnail.
+    takes each url in the list downloads it and then processes what to do with it.
+    """
     for (url, thumb) in urls:
         try:
             url = urlparse(url, scheme='http').geturl()
@@ -111,20 +110,26 @@ def process_urls(urls, lmdb_env):
                 if img_resp.status_code != 200:
                     raise ValueError('failure to grab both image and thumbnail')
             content = img_resp.content
-        except:
+            hash_val = hashlib.sha256(content)
+            if not dedup(hash_val, lmdb_env):
+                write_new(url, content, hash_val, lmdb_env)
+            else:
+                print('Duplicate image detected')
+        except requests.exceptions.RequestException:
             print('ERROR')
             print(traceback.format_exc())
-        h = hashlib.sha256(content)
-        if not dedup(h, lmdb_env):
-            write_new(url, content, h, lmdb_env)
-        else:
-            print('Duplicate image detected')
+
 
 
 def main():
-    custom_search_engine_id, google_api_key = grab_auth()    
+    """
+    main entry point into the program,
+    drives the process of going to google, downloading the images, etc.
+    """
+    custom_search_engine_id, google_api_key = grab_auth()
     if custom_search_engine_id is None or google_api_key is None:
-        print('please provide CSE ID and API Key via environment variables (GOOGLE_API_CX and GOOGLE_API_KEY)')
+        print('please provide CSE ID and API Key via ' +
+              'environment variables (GOOGLE_API_CX and GOOGLE_API_KEY)')
         sys.exit(1)
     if not os.path.exists(GOOGLE_API_OUTPUT_DIR):
         os.makedirs(GOOGLE_API_OUTPUT_DIR)
@@ -134,12 +139,12 @@ def main():
     lmdb_env = lmdb.open(LMDB_FILE, max_dbs=1, map_size=(1000 * 1000 * 1000))
 
     start = 1
-    while True:
+    while start < 100:
         payload = execute_request(custom_search_engine_id, google_api_key, start=start)
         t_process = time.time()
         print('%s\n' % t_process)
-        with open ('%s/%s' % (GOOGLE_API_OUTPUT_DIR, t_process), 'w') as f:
-            json.dump(payload, f)
+        with open('%s/%s' % (GOOGLE_API_OUTPUT_DIR, t_process), 'w') as search_res_file:
+            json.dump(payload, search_res_file)
 
         # process_payload(payload)
         urls = get_urls(payload)
