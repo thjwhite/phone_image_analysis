f98d14794c42e6d1229ac702a66d7ae06588627e
tj.white <thjwhite@gmail.com>
2017-07-11 19:11:34 -0700

    organize photos into ios vs android. include android search.

--- pull_images.py
+++ pull_images.py
@@ -19,6 +19,11 @@ GOOGLE_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1'
 GOOGLE_API_OUTPUT_DIR = '.google_api_queries'
 IMAGE_REPO_DIR = '.images'
 LMDB_FILE = '.registrar'
+IOS_CLASS = 'ios'
+ANDROID_CLASS = 'android'
+IOS_SEARCH_TERMS = ['iphone', 'iphone 7', 'iphone 6', 'ios screenshot', 'ipad']
+ANDROID_SEARCH_TERMS = ['android phone', 'android screenshot',
+                        'samsung galaxy', 'google pixel', 'android tablet']
 
 
 def grab_auth():
@@ -30,24 +35,24 @@ def grab_auth():
     return custom_search_engine_id, google_api_key
 
 
-def generate_iphone_search_params(engine_id, api_key, start=1):
+def generate_iphone_search_params(term, engine_id, api_key, start=1):
     """
-    parameters for a search for iphones
+    parameters for a search for phone images
     """
     params = dict()
     params['cx'] = engine_id  # custom search engine identifier
     params['key'] = api_key  # api key
     params['searchType'] = 'image'  # tell google we want images, not links
-    params['q'] = 'iphone'  # our search term
+    params['q'] = term  # our search term
     params['start'] = start  # pagination start
     return params
 
 
-def execute_request(custom_search_engine_id, google_api_key, start=1):
+def execute_request(term, custom_search_engine_id, google_api_key, start=1):
     """
     execute request to google for the results
     """
-    params = generate_iphone_search_params(custom_search_engine_id, google_api_key, start)
+    params = generate_iphone_search_params(term, custom_search_engine_id, google_api_key, start)
     response = requests.get(GOOGLE_SEARCH_URL, params=params)
     print(response)
     if response.status_code != 200:
@@ -71,12 +76,12 @@ def get_urls(payload):
     return urls
 
 
-def write_new(url, content, hash_val, lmdb_env):
+def write_new(url, content, classification, hash_val, lmdb_env):
     """
     writes out a new image
     """
     uuid_val = uuid.uuid4()
-    with open('%s/%s' % (IMAGE_REPO_DIR, uuid_val), 'wb') as image_file:
+    with open('%s/%s/%s' % (IMAGE_REPO_DIR, classification, uuid_val), 'wb') as image_file:
         image_file.write(content)
     print('wrote url=%s\n      uuid=%s\n      sha256=%s\n' %
           (url, str(uuid_val), hash_val.hexdigest()))
@@ -94,7 +99,7 @@ def dedup(hash_val, lmdb_env):
         return image_uuid is not None
 
 
-def process_urls(urls, lmdb_env):
+def process_urls(urls, classification, lmdb_env):
     """
     urls is a list of tuples (url, thumbnail). We try the url first,
     but if it fails to download it we can use the thumbnail.
@@ -112,13 +117,31 @@ def process_urls(urls, lmdb_env):
             content = img_resp.content
             hash_val = hashlib.sha256(content)
             if not dedup(hash_val, lmdb_env):
-                write_new(url, content, hash_val, lmdb_env)
+                write_new(url, content, classification, hash_val, lmdb_env)
             else:
                 print('Duplicate image detected')
         except requests.exceptions.RequestException:
             print('ERROR')
             print(traceback.format_exc())
 
+
+def process_term(term, classification, custom_search_engine_id, google_api_key, lmdb_env):
+    """
+    for a particular term go out to google, and then download the images
+    """
+    start = 1
+    while start < 100:
+        payload = execute_request(term, custom_search_engine_id, google_api_key, start=start)
+        t_process = time.time()
+        print('%s\n' % t_process)
+        with open('%s/%s' % (GOOGLE_API_OUTPUT_DIR, t_process), 'w') as search_res_file:
+            json.dump(payload, search_res_file)
+
+        # process_payload(payload)
+        urls = get_urls(payload)
+        process_urls(urls, classification, lmdb_env)
+
+        start = payload['queries']['nextPage'][0]['startIndex']
 
 
 def main():
@@ -131,27 +154,23 @@ def main():
         print('please provide CSE ID and API Key via ' +
               'environment variables (GOOGLE_API_CX and GOOGLE_API_KEY)')
         sys.exit(1)
+
     if not os.path.exists(GOOGLE_API_OUTPUT_DIR):
         os.makedirs(GOOGLE_API_OUTPUT_DIR)
     if not os.path.exists(IMAGE_REPO_DIR):
         os.makedirs(IMAGE_REPO_DIR)
+    if not os.path.exists('%s/%s' % (IMAGE_REPO_DIR, IOS_CLASS)):
+        os.makedirs('%s/%s' % (IMAGE_REPO_DIR, IOS_CLASS))
+    if not os.path.exists('%s/%s' % (IMAGE_REPO_DIR, ANDROID_CLASS)):
+        os.makedirs('%s/%s' % (IMAGE_REPO_DIR, ANDROID_CLASS))
 
     lmdb_env = lmdb.open(LMDB_FILE, max_dbs=1, map_size=(1000 * 1000 * 1000))
 
-    start = 1
-    while start < 100:
-        payload = execute_request(custom_search_engine_id, google_api_key, start=start)
-        t_process = time.time()
-        print('%s\n' % t_process)
-        with open('%s/%s' % (GOOGLE_API_OUTPUT_DIR, t_process), 'w') as search_res_file:
-            json.dump(payload, search_res_file)
+    for term in IOS_SEARCH_TERMS:
+        process_term(term, IOS_CLASS, custom_search_engine_id, google_api_key, lmdb_env)
 
-        # process_payload(payload)
-        urls = get_urls(payload)
-        process_urls(urls, lmdb_env)
-
-        start = payload['queries']['nextPage'][0]['startIndex']
-
+    for term in ANDROID_SEARCH_TERMS:
+        process_term(term, ANDROID_CLASS, custom_search_engine_id, google_api_key, lmdb_env)
 
 if __name__ == "__main__":
     main()
